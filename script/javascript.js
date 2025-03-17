d6762039f6431074c552049e02662abbffb14a61
IsaacWang1125 <1532992@stu.tmc.ac.uk>
2025-03-17 15:10:22 +0000

    Update javascript.js

--- script/javascript.js
+++ script/javascript.js
@@ -1,2 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
   const image = document.querySelector('.blur-image');
+
+  image.addEventListener('click', function () {
+    alert('Image clicked!');
